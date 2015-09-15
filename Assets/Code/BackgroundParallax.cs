efe5c9cd48148e2942dbdfc175a790237580d4e3
= <=>
2015-09-15 12:49:02 +0100

    add background parallax script

---
+++ Assets/Code/BackgroundParallax.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundParallax : MonoBehaviour {
+
+	public Transform background;
+	public float parallaxScale;
+
+	private Vector3 _lastPosition;
+
+	void Start () {
+		_lastPosition = transform.position;
+	}
+
+	void Update () {
+		var parallax = (_lastPosition.x - transform.position.x) * parallaxScale;
+		var backgroundTargetPosition = background.position.x + parallax;
+		background.position = new Vector3 (backgroundTargetPosition, background.position.y, background.position.z);
+		_lastPosition = transform.position;
+	}
+}
