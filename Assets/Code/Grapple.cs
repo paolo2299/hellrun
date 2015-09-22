9a10b735971f5f17a97421d04f3996d28a3484de
= <=>
2015-09-22 22:58:52 +0100

    use sprite for grapple instead of LineRenderer

--- Assets/Code/Grapple.cs
+++ Assets/Code/Grapple.cs
@@ -7,11 +7,13 @@ public class Grapple : MonoBehaviour {
 
 	private Vector2 _anchor;
 	private Vector2 _trailingEnd;
-	private LineRenderer _lineRenderer;
+	private SpriteRenderer _spriteRenderer;
+	private float _normalisedLength;
 
 	public void Awake () {
-		_lineRenderer = GetComponent<LineRenderer>();
-		_lineRenderer.sortingLayerName = "grapple";
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+		var spriteBounds = _spriteRenderer.bounds;
+		_normalisedLength = spriteBounds.max.y - spriteBounds.min.y;
 	}
 
 	public void SetEnds (Vector2 anchor, Vector2 trailingEnd) {
@@ -19,13 +21,28 @@ public class Grapple : MonoBehaviour {
 		_trailingEnd = trailingEnd;
 	}
 
+	public float AngleInDegrees () {
+		var opposite = transform.position.x - _anchor.x;
+		var adjacent = _anchor.y - transform.position.y;
+		if (opposite == 0)
+			return 0;
+		var ratio = opposite / adjacent;
+		return Mathf.Atan (ratio) * (360 / (2 * Mathf.PI));
+	}
+
+	public float LengthMultiplier () {
+		var length = (_anchor - _trailingEnd).magnitude;
+		return 1.56f * length / _normalisedLength; //TODO why 1.56? figure out what's going on here...
+	}
+
 	public void LateUpdate () {
 		if (isActive) {
-			_lineRenderer.enabled = true;
-			_lineRenderer.SetPosition (0, new Vector3(_anchor.x, _anchor.y, -1));
-			_lineRenderer.SetPosition (1, new Vector3(_trailingEnd.x, _trailingEnd.y, -1));
+			_spriteRenderer.enabled = true;
+			transform.position = _trailingEnd;
+			transform.eulerAngles = new Vector3(0, 0, AngleInDegrees());
+			transform.localScale = new Vector3(transform.localScale.x, _normalisedLength * LengthMultiplier(), transform.localScale.z);
 		} else {
-			_lineRenderer.enabled = false;
+			_spriteRenderer.enabled = false;
 		}
 	}
 }
