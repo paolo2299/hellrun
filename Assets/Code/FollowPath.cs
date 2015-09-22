e2631fb827d3c1d33c533f0365168b73bc514e8b
= <=>
2015-09-22 23:22:22 +0100

    moving platforms WIP

---
+++ Assets/Code/FollowPath.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System
+
+public class FollowPath : MonoBehaviour {
+
+	public PathDefinition path;
+	public float speed = 1;
+	public float MaxDistanceToGoal = .1f;
+
+	private IEnumerator<Transform> _currentPoint;
+	
+	public void Start () {
+		if (path == null) {
+			Debug.LogError("Path cannot be null", gameObject);
+			return;
+		}
+
+		_currentPoint = path.GetPathEnumerator ();
+		_currentPoint.MoveNext ();
+
+		if (_currentPoint.Current == null)
+			return;
+
+		transform.position = _currentPoint.Current.position;
+	}
+
+	public void Update () {
+		if (_currentPoint == null || _currentPoint.Current == null)
+			return;
+
+		transform.position = Vector3.MoveTowards (transform.position, _currentPoint.Current.position, Time.deltaTime * speed);
+
+		var distanceSquared = (transform.position - _currentPoint.Current.position).sqrMagnitude;
+
+		//TODO - rest of method
+	}
+}
---
+++ Assets/Code/PathDefinition.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathDefinition : MonoBehaviour {
+
+	public Transform[] points;
+
+	public IEnumerator<Transform> GetPathEnumerator() {
+		if (points == null || points.Length < 1)
+			yield break;
+
+		var direction = 1;
+		var index = 0;
+		while (true) {
+			yield return points[index];
+
+			if (points.Length == 1)
+				continue;
+
+			if (index <= 0)
+				direction = 1;
+			else if (index >= points.Length - 1)
+				direction = -1;
+
+			index = index + direction;
+		}
+	}
+
+	public void OnDrawGizmos() {
+		if (points == null || points.Length < 2)
+			return;
+
+		for (var i = 1; i < points.Length; i++) {
+			Gizmos.DrawLine(points[i - 1].position, points[i].position);
+		}
+	}
+}
