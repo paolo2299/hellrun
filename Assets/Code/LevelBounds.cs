5917df8173d2e8d3d424aa778b93ef4a04626da1
= <=>
2015-09-26 15:37:23 +0100

    kill the player when they leave the level bounds

---
+++ Assets/Code/LevelBounds.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBounds : MonoBehaviour {
+	public void OnTriggerExit2D(Collider2D other)
+	{
+		var player = other.gameObject.GetComponent<Player> ();
+		if (player != null) {
+			LevelManager.Instance.KillPlayer();
+		}
+	}
+}
