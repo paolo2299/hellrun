181e65d9a964eb884fa2a04ac55d562ddd23f6d3
= <=>
2015-10-04 14:39:01 +0100

    improve formatting of the displayed level time

--- Assets/Code/LevelManager.cs
+++ Assets/Code/LevelManager.cs
@@ -7,11 +7,17 @@ public class LevelManager : MonoBehaviour {
 
 	public Player player;
 	public CameraController mainCamera;
+	public GUIText elapsedTimeText;
+	public GUIText levelNameText;
+	private float elapsedTimeThisTry;
+	private float totalElapsedTime;
 
 	public void Awake() {
 		LevelManager.Instance = this;;
 		player = GameObject.FindObjectOfType<Player> ();
 		mainCamera = GameObject.FindObjectOfType<CameraController> ();
+		elapsedTimeThisTry = 0f;
+		totalElapsedTime = 0f;
 	}
 
 	public void GotoNextLevel(string levelName) {
@@ -35,6 +41,23 @@ public class LevelManager : MonoBehaviour {
 	private IEnumerator KillPlayerCo() {
 		player.Die ();
 		yield return new WaitForSeconds (0.5f);
+		elapsedTimeThisTry = 0f;
 		player.Respawn ();
+	}
+
+	public void Update() {
+		elapsedTimeThisTry += Time.deltaTime;
+		totalElapsedTime += Time.deltaTime;
+		elapsedTimeText.text = ElapsedTimeThisTryString ();
+		if (totalElapsedTime > 5f) {
+			levelNameText.enabled = false;
+		}
+	}
+
+	public string ElapsedTimeThisTryString() {
+		var minutes = (int)(elapsedTimeThisTry / 60);
+		var seconds = (int)(elapsedTimeThisTry % 60);
+		var fraction = (int)((elapsedTimeThisTry * 100) % 100);
+		return string.Format ("{0:00}:{1:00}:{2:00}", minutes, seconds, fraction);
 	}
 }
