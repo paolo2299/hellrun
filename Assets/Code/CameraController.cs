fd2169a56f7c06c7a12922a796b524eda53fc39e
= <=>
2015-11-27 20:43:03 +0000

    fix the aspect ratio and letterbox where necessary

--- Assets/Code/CameraController.cs
+++ Assets/Code/CameraController.cs
@@ -7,11 +7,48 @@ public class CameraController : MonoBehaviour {
 	public BoxCollider2D Bounds;
 	public bool IsFollowing = true;
 
+	private float targetaspectX = 16.0f;
+	private float targetaspectY = 9.0f;
+	private float targetaspect;
+
+	private Camera camera;
+
 	private Vector3 
 		_min,
 		_max;
 
-	void Start () {;
+	void Start () {
+		camera = GetComponent<Camera> ();
+
+		//set the desired aspect ratio
+		targetaspect = targetaspectX / targetaspectY;
+
+		//determine the game window's current aspect ratio
+		float windowaspect = (float)Screen.width / (float)Screen.height;
+
+		float scaleHeight = windowaspect / targetaspect;;
+
+		if (scaleHeight < 1.0f) {
+			Rect rect = camera.rect;
+
+			rect.width = 1.0f;
+			rect.height = scaleHeight;
+			rect.x = 0;
+			rect.y = (1.0f - scaleHeight) / 2.0f;
+
+			camera.rect = rect;
+		} else {
+			float scaleWidth = 1.0f / scaleHeight;
+
+			Rect rect = camera.rect;
+			rect.width = scaleWidth;
+			rect.height = 1.0f;
+			rect.x = (1.0f - scaleWidth) / 2.0f;
+			rect.y = 0;
+
+			camera.rect = rect;
+		}
+
 		_min = Bounds.bounds.min;
 		_max = Bounds.bounds.max;
 	}
@@ -26,7 +63,7 @@ public class CameraController : MonoBehaviour {
 			y = Player.position.y;
 		}
 
-		var cameraHalfWidth = Camera.main.orthographicSize * ((float) Screen.width / Screen.height);
+		var cameraHalfWidth = Camera.main.orthographicSize * targetaspect;
 
 		x = Mathf.Clamp (x, _min.x + cameraHalfWidth, _max.x - cameraHalfWidth);
 		y = Mathf.Clamp (y, _min.y + Camera.main.orthographicSize, _max.y - Camera.main.orthographicSize);
