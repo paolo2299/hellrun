af500f1e934bb8f160233ee375ab05ad3b5a613e
= <=>
2016-01-05 08:35:32 +0000

    set up the menu screen ready for player recording

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
+	//set all of these when making a player recording
+	public bool playerRecordingMode = false;
+	public Player player;
+
 	// Use this for initialization
 	void Start () {
-	
+		if (playerRecordingMode) {
+			player.CollectGrapple ();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space)) {
+		if (!playerRecordingMode && (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space))) {
 			Application.LoadLevel("level_select");
 		}
 	}
