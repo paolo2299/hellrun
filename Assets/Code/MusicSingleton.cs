1dffc7d8fba227a4fac150ffeb7a3abad6a17e4c
= <=>
2016-02-24 08:12:42 +0000

    add different music for levels than game menu and level select

--- Assets/Code/MusicSingleton.cs
+++ Assets/Code/MusicSingleton.cs
@@ -3,20 +3,29 @@ using System.Collections;
 
 public class MusicSingleton : MonoBehaviour {
 
+	public AudioSource audio;
+
 	private static MusicSingleton instance = null;
 	public static MusicSingleton Instance {
 		get { return instance; }
-	}	
+	}
 
 	void Awake() {
 		if (instance != null && instance != this) {
+			AudioSource existingAudio = instance.audio;;
+			if (existingAudio.clip.name != audio.clip.name) {
+				existingAudio.Stop();
+				existingAudio.clip = audio.clip;
+				existingAudio.Play();
+			}
 			Destroy(this.gameObject);
+			Destroy(audio.gameObject);
 			return;
 		} else {
 			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+			DontDestroyOnLoad(audio.gameObject);
+			audio.Play();
 		}
-		DontDestroyOnLoad(this.gameObject);
-		AudioSource audio = GetComponent<AudioSource>();
-		audio.Play();
 	}
 }	
