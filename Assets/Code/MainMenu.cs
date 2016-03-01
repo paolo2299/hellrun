9ea5a33c9befa648d0998909d4e5b59bb7d40549
= <paolo@example.com>
2016-03-01 19:06:23 +0000

    complete basic implementation of main menu

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
@@ -13,11 +14,25 @@ public class MainMenu : MonoBehaviour {
 			player.CollectGrapple ();
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!playerRecordingMode && (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space))) {
-			Application.LoadLevel("level_select");
-		}
+
+	public void ButtonSelected(Button button) {
+		var text = button.GetComponentInChildren<Text> ();
+		text.color = new Color (1f, 1f, 1f, 1f);
+	}
+
+	public void ButtonDeselected(Button button) {
+		var text = button.GetComponentInChildren<Text> ();
+		text.color = new Color (0.412f, 0.412f, 0.412f, 1f);
+	}
+
+	public void StartGame(Button button) {
+		var text = button.GetComponentInChildren<Text> ();
+		text.color = new Color (1f, 1f, 0f, 1f);
+		Application.LoadLevel ("level_select");
+	}
+
+	public void QuitGame(Button button) {
+		var text = button.GetComponentInChildren<Text> ();
+		text.color = new Color (1f, 1f, 0f, 1f);
 	}
 }
