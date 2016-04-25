c60677458417406a7178453b379299df66b4c90b
= <paolo@example.com>
2016-04-25 08:17:45 +0100

    latest

--- Assets/Code/RandomSpawner.cs
+++ Assets/Code/RandomSpawner.cs
@@ -7,33 +7,60 @@ public class RandomSpawner : MonoBehaviour {
 
 	public Transform spawnPoint;
 	public GameObject template;
+
 	public float spawnFrequency = 10f;
 	public float spawnProbability = 0.05f;
-	public float delay = 0f;
-	
-	private float _lastSpawnAttempt;
+
+	private static int spawnArraySize = 10;
+	private float startTime;
+	private float spawnTimeOffset = 0f;
+	private float[] spawnTimes = new float[spawnArraySize];
+	private int spawnTimesIndex = 0;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Time.time - _lastSpawnAttempt > 1f / spawnFrequency) {
-			MaybeSpawn();
+		startTime = Time.fixedTime;
+
+		Random.seed = 42;
+		var index = 0;
+		var time = 0f;
+		while (index < spawnArraySize) {
+			var p = Random.value;
+			if (p < spawnProbability) {
+				spawnTimes[index] = time;
+				Debug.Log (time);
+				index += 1;
+			}
+			time += 1f / spawnFrequency;
 		}
 	}
 
-	void MaybeSpawn() {
-		var p = Random.value;
-		if (p < spawnProbability) {
-			Spawn();
+	void FixedUpdate () {
+		if (ShouldSpawn ()) {
+			Spawn ();
 		}
-		_lastSpawnAttempt = Time.time;
 	}
 
-	void Spawn() {
+	private bool ShouldSpawn() {
+		var result = false;
+		while (spawnTimes[spawnTimesIndex] + spawnTimeOffset < Time.fixedTime) {
+			result = true;
+			IncrementIndex();
+		}
+		return result;
+	}
+
+	private void IncrementIndex() {
+		if (spawnTimesIndex == spawnArraySize - 1) {
+			spawnTimesIndex = 0;
+			spawnTimeOffset = Time.fixedTime;
+		} else {
+			spawnTimesIndex += 1;
+			Debug.Log (spawnTimesIndex);
+		}
+	}
+
+	private void Spawn() {
 		var ob = Object.Instantiate (template);
 		ob.tag = "Destroyable";
 		ob.transform.position = spawnPoint.position;
--- Assets/Code/SemiCollidableSwitch.cs
+++ Assets/Code/SemiCollidableSwitch.cs
@@ -4,17 +4,33 @@ using System.Collections;
 public class SemiCollidableSwitch : MonoBehaviour {
 
 	public float semiProbability = 0.5f;
+	
+	private string[] switchValues = new string[100];
+	private int switchValueIndex = 0;
+
+	void Start () {
+		Random.seed = 42;
+		for (int i = 0; i < 100; i++) {
+			var p = Random.value;
+			if (p < semiProbability) {
+				switchValues[i] = "SemiCollidablePasser";
+			} else {
+				switchValues[i] = "SemiCollidableNonPasser";
+			}
+		}
+	}
+
+	private string getNextSwitchValue() {
+		var value = switchValues [switchValueIndex];
+		switchValueIndex = (switchValueIndex + 1) % 100;
+		return value;
+	}
 
 	public void OnTriggerEnter2D(Collider2D other)
 	{
 		var layer = LayerMask.LayerToName(other.gameObject.layer);
 		if (layer == "SemiCollidablePasser" || layer == "SemiCollidableNonPasser") {
-			var p = Random.value;
-			if (p < semiProbability) {
-				other.gameObject.layer = LayerMask.NameToLayer("SemiCollidablePasser");
-			} else {
-				other.gameObject.layer = LayerMask.NameToLayer("SemiCollidableNonPasser");
-			}
+			other.gameObject.layer = LayerMask.NameToLayer(getNextSwitchValue());
 		}
 	}
 }
